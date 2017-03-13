00516025b6825b90cab3c553da7fdf42d8c5cb24
Taylor Scott <TaylorScott@Taylors-MacBook-Pro.local>
2017-03-13 10:08:19 -0400

    some controllers

--- app/controllers/chasers_controller.rb
+++ app/controllers/chasers_controller.rb
@@ -1,2 +1,46 @@
 class ChasersController < ApplicationController
+  def index
+    @shot = Shot.find(params[:id])
+    @chasers = @shot.chasers.all
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @chaser }
+    end
+  end
+
+  def new
+    @shot = Shot.find(params[:id])
+    @chaser = @shot.chasers.new
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @chaser }
+    end
+  end
+
+  def create
+    @shot = Shot.find(params[:id])
+    @chaser = Chaser.create!(chaser_params)
+    redirect_to shot_chasers_path(@shot)
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @chaser }
+    end
+  end
+
+  def show
+    @chaser = Chaser.find(params[:id])
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @chaser }
+    end
+  end
+
+  def chaser_params
+    params.require(:chaser).permit(:author, :content)
+  end
+
 end
--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -1,2 +1,45 @@
 class TabsController < ApplicationController
+
+  def index
+    @tabs = Tab.all
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @tab }
+    end
+  end
+
+  def new
+    @tab = Tab.new
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @tab }
+    end
+  end
+
+  def create
+    @tab = Tab.create!(tab_params)
+    redirect_to tab_path(@tab)
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @tab }
+    end
+  end
+
+  def show
+    @tab = Tab.find(params[:id])
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @tab }
+    end
+  end
+
+  def tab_params
+    params.require(:tab).permit(:name, :description)
+  end
+
+
 end
