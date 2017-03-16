147cd815303b1b08b0565b23639a98fc701c3d4e
Samuel Clements <sdkclements@gmail.com>
2017-03-16 09:14:43 -0400

    working on deployment

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module OneShotBackend
 	config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*' 
+        origins 'https://one-shot-backend.herokuapp.com/tabs' 
         #this allows any origin. for testing purposes this is fine, but for deployment, you'll want to permit your `gh-pages`-deployed front end to interact with your API
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
