d9fe0debef578873d2663a1a3bc4e8791dac749a
Samuel Clements <sdkclements@gmail.com>
2017-03-16 10:25:46 -0400

    got create working

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,66 +1,67 @@
 class ShotsController < ApplicationController
-  def index
+ def index
 
-  	@tab = Tab.find(params[:tab_id])
-    @shots = @tab.shots.all
+     @tab = Tab.find(params[:tab_id])
+   @shots = @tab.shots.all
 
-    render json: @shots, status: :ok
-  end
+   render json: @shots, status: :ok
+ end
 
-  def show
-
-
-  	@tab = Tab.find(params[:id])
-    @shots = @tab.shots.find(params[:id])
-    render json: @shot.to_json, include: :chasers
-    # respond_to do |format|
-    #   format.html {render :index}
-    #   format.json {render json: @shots}
-    # end
-  end
+ def show
 
 
-
-
-  def new
-    @tab = Tab.find(params[:tab_id])
-    @shot = Shot.new
-
-    respond_to do |format|
-      format.html {render :show}
-      format.json {render json: @shot}
-    end
-  end
-
-  def create
-
-  	@tab = Tab.find(params[:tab_id])
-    @shot = Shot.new(shot_params)
-    # respond_to do |format|
-    #   if @shot.save
-    #     format.html { redirect_to @shot, notice: "made the shot"}
-    #     format.json { render json: @shot, status: :created, location: @shot}
-    #   else
-    #     format.html { render :new}
-    #     format.json {render json: @shot.errors, status: :unprocessable_entity}
-    #   end
-    # end
+     @tab = Tab.find(params[:id])
+   @shots = @tab.shots.find(params[:id])
+   render json: @shot.to_json, include: :chasers
+   # respond_to do |format|
+   #   format.html {render :index}
+   #   format.json {render json: @shots}
+   # end
+ end
 
 
 
-      if @shot.save
-        render json: @shot, status: :created, location: @shot
-      else
-       render json: @shot.errors, status: :unprocessable_entity
-      end
+
+ def new
+   @tab = Tab.find(params[:tab_id])
+   @shot = Shot.new
+
+   respond_to do |format|
+     format.html {render :show}
+     format.json {render json: @shot}
+   end
+ end
+
+ def create
+
+     @tab = Tab.find(params[:tab_id])
+   @shot = Shot.new(shot_params)
+   # respond_to do |format|
+   #   if @shot.save
+   #     format.html { redirect_to @shot, notice: "made the shot"}
+   #     format.json { render json: @shot, status: :created, location: @shot}
+   #   else
+   #     format.html { render :new}
+   #     format.json {render json: @shot.errors, status: :unprocessable_entity}
+   #   end
+   # end
 
 
-  end
 
-  private
+   if @shot.save
+     puts "Saved"
+     puts "=" * 55
+     render json: @shot, status: :created#, location: @shot
+   else
+    render json: @shot.errors, status: :unprocessable_entity
+   end
 
-  def shot_params
-    params.require(:shot).permit(:title, :author, :content)
-  end
+ end
+
+ private
+
+ def shot_params
+   params.require(:shot).permit(:title, :author, :content, :tab_id)
+ end
 
 end
