41068f9edb919bad549ce1bd1d9b062679b0b130
superbuggy <jpqreichard@gmail.com>
2017-04-03 14:07:42 -0400

    add james' comments & eval

--- app/controllers/chasers_controller.rb
+++ app/controllers/chasers_controller.rb
@@ -1,4 +1,7 @@
 class ChasersController < ApplicationController
+  # one can use before_action to DRY up your controller code, but
+  # http://blog.thefrontiergroup.com.au/2014/02/before_action_an_anti_pattern/
+
   def index
     @tab = Tab.find(params[:tab_id])
     @shot = Shot.find(params[:shot_id])
@@ -11,15 +14,15 @@ class ChasersController < ApplicationController
     @tab = Tab.find(params[:tab_id])
     @shot = Shot.find(params[:shot_id])
     @chaser = Chaser.find(params[:id])
-
     render json: @chaser.to_json, status: :ok
   end
 
+  # don't need new here. the function of using new and edit is to render views
+  # update
   def new
     @tab = Tab.find(params[:tab_id])
     @shot = Shot.find(params[:shot_id])
     @chaser = Chasers.new
-
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @chaser }
@@ -30,8 +33,7 @@ class ChasersController < ApplicationController
     @tab = Tab.find(params[:tab_id])
     @shot = Shot.find(params[:shot_id])
     @chaser = Chaser.create!(chaser_params)
-    redirect_to shot_chasers_path(@shot)
-
+    redirect_to shot_chasers_path @shot
     render json: @chasers, status: :ok
   end
 
--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,67 +1,41 @@
 class ShotsController < ApplicationController
- def index
+  def index
+    @tab = Tab.find(params[:tab_id])
+    @shots = @tab.shots.all
+    render json: @shots, status: :ok
+  end
 
-     @tab = Tab.find(params[:tab_id])
-   @shots = @tab.shots.all
+  def show
+    @tab = Tab.find(params[:id])
+    @shots = @tab.shots.find(params[:id])
+    render json: @shot.to_json, include: :chasers
+  end
 
-   render json: @shots, status: :ok
- end
+  # see other controller comments
+  def new
+    @tab = Tab.find(params[:tab_id])
+    @shot = Shot.new
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @shot}
+    end
+  end
 
- def show
+  def create
+    @tab = Tab.find(params[:tab_id])
+    @shot = Shot.new(shot_params)
+    if @shot.save
+      # remove debugging puts statements
+      puts "Saved"
+      puts "=" * 55
+      render json: @shot, status: :created#, location: @shot
+    else
+      render json: @shot.errors, status: :unprocessable_entity
+    end
+  end
 
-
-     @tab = Tab.find(params[:id])
-   @shots = @tab.shots.find(params[:id])
-   render json: @shot.to_json, include: :chasers
-   # respond_to do |format|
-   #   format.html {render :index}
-   #   format.json {render json: @shots}
-   # end
- end
-
-
-
-
- def new
-   @tab = Tab.find(params[:tab_id])
-   @shot = Shot.new
-
-   respond_to do |format|
-     format.html {render :show}
-     format.json {render json: @shot}
-   end
- end
-
- def create
-
-     @tab = Tab.find(params[:tab_id])
-   @shot = Shot.new(shot_params)
-   # respond_to do |format|
-   #   if @shot.save
-   #     format.html { redirect_to @shot, notice: "made the shot"}
-   #     format.json { render json: @shot, status: :created, location: @shot}
-   #   else
-   #     format.html { render :new}
-   #     format.json {render json: @shot.errors, status: :unprocessable_entity}
-   #   end
-   # end
-
-
-
-   if @shot.save
-     puts "Saved"
-     puts "=" * 55
-     render json: @shot, status: :created#, location: @shot
-   else
-    render json: @shot.errors, status: :unprocessable_entity
-   end
-
- end
-
- private
-
- def shot_params
-   params.require(:shot).permit(:title, :author, :content, :tab_id)
- end
-
+  private
+  def shot_params
+    params.require(:shot).permit(:title, :author, :content, :tab_id)
+  end
 end
